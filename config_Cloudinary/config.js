2a7d9d5e437c68a104c87e92d0793ae35623c0e1
JoseMR <jose8188258@gmail.com>
2022-01-09 22:38:30 -0400

    #UpdateImagesCloudinary

--- config_Cloudinary/config.js
+++ config_Cloudinary/config.js
@@ -2,7 +2,6 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: './models',
     filename: function(req, file, cb) {
         cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
     }
